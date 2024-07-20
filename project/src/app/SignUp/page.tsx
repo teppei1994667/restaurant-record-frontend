1cb5fdb1c72a7e482a0519dab93171989decda2d
teppei1994667 <teppei.engineer.667@gmail.com>
2024-07-20 11:27:44 +0900

    SignUpページの基本レイアウト作成

---
+++ project/src/app/SignUp/page.tsx
@@ -0,0 +1,25 @@
+import { Header } from "@/share/components/Header";
+import { Grid, Typography } from "@mui/material";
+import { SignUpForm } from "./components/signUpForm";
+
+const SignUp = () => {
+  return (
+    <>
+      <Header />
+      <Grid container className="justify-center mt-10">
+        <Grid item>
+          <Typography className="text-gray-500 font-mono" variant="h4">
+            新規登録
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container className="justify-center mt-10">
+        <Grid item>
+          <SignUpForm />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default SignUp;
