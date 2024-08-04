02f7bee655631d1e2886cf7a7b5710762ed44e0c
teppei1994667 <teppei.engineer.667@gmail.com>
2024-08-04 22:05:25 +0900

    FormPasswordTextField 作成

---
+++ project/src/share/form/FormPasswordTextField.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { forwardRef, useState } from "react";
+import { IconButton, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { TextFieldProps } from "./type/ReactHookFormType";
+import { FormTextField } from "./FormTextField";
+
+export const FormPasswordTextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
+  const { rules, ...restProps } = props;
+  const passwordRegExp = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}$/;
+
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  // テキストフィールド内のパスワード表示アイコン押下時
+  const handleShowPasswordOnClick = () => {
+    setIsShowPassword((prev) => !prev);
+  };
+
+  //PasswordTextField標準のバリデーションルール
+  const passwordTextFieldRules = {
+    required: { value: true, message: "必須入力です" },
+    maxLength: {
+      value: 100,
+      message: "パスワードは10桁以上、100桁以下で入力してください",
+    },
+    minLength: {
+      value: 10,
+      message: "パスワードは10桁以上、100桁以下で入力してください",
+    },
+    pattern: {
+      value: passwordRegExp,
+      message:
+        "パスワードには半角数字、半角英子文字、半角英大文字のみ使用可能です。また各種最低１桁以上含めてください",
+    },
+    //呼び出し元からの上書き可能
+    ...rules,
+  };
+  return (
+    <>
+      <FormTextField
+        rules={passwordTextFieldRules}
+        type={isShowPassword ? "text" : "password"}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleShowPasswordOnClick}
+                edge="end"
+              >
+                {isShowPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        {...restProps}
+        ref={ref}
+      />
+    </>
+  );
+});
+
+FormPasswordTextField.displayName = "FormPasswordTextField";
