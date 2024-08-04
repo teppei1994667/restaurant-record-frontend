852a95e318f48cde3b411429b826f331ca7e08eb
teppei1994667 <teppei.engineer.667@gmail.com>
2024-08-04 23:04:15 +0900

    SignUpページ 入力フォーム作成

--- project/src/app/SignUp/components/SignUpForm.tsx
+++ project/src/app/SignUp/components/SignUpForm.tsx
@@ -1,64 +1,89 @@
-import { Paper, Grid, Button, TextField } from "@mui/material";
+"use client";
+
+import { FormEmailTextField } from "@/share/form/FormEmailTextField";
+import { FormPasswordTextField } from "@/share/form/FormPasswordTextField";
+import { FormTextField } from "@/share/form/FormTextField";
+import { Paper, Grid, Button } from "@mui/material";
+import { FormProvider, useForm } from "react-hook-form";
 
 export const SignUpForm = () => {
+  const signUpForm = useForm({
+    defaultValues: { name: "", email: "", password: "", passwordConfirmation: "" },
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+  });
+
+  const nameRules = {
+    required: { value: true, message: "必須入力です" },
+  };
+
+  // 登録ボタンテスト用関数
+  const handleSignUpOnClickTest = () => {
+    console.log("登録", signUpForm.getValues());
+    signUpForm.reset();
+  };
   return (
     <>
-      <Paper elevation={0} sx={{ height: "100vh" }}>
-        <Grid container>
-          <Grid item className="w-96">
-            <TextField
-              name="name"
-              label="ユーザー名"
-              fullWidth
-              InputLabelProps={{ className: "font-mono" }}
-            />
+      <FormProvider {...signUpForm}>
+        <Paper elevation={0} sx={{ height: "100vh" }}>
+          <Grid container>
+            <Grid item className="w-96">
+              <FormTextField
+                name="name"
+                label="ユーザー名"
+                rules={nameRules}
+                helperText={signUpForm.formState.errors.name?.message}
+                InputLabelProps={{ className: "font-mono" }}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container className="mt-10">
-          <Grid item className="w-96">
-            <TextField
-              name="email"
-              label="メールアドレス"
-              fullWidth
-              InputLabelProps={{ className: "font-mono" }}
-            />
+          <Grid container className="mt-10">
+            <Grid item className="w-96">
+              <FormEmailTextField
+                name="email"
+                label="メールアドレス"
+                helperText={signUpForm.formState.errors.email?.message}
+                InputLabelProps={{ className: "font-mono" }}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container className="mt-10">
-          <Grid item className="w-96">
-            <TextField
-              name="password"
-              label="パスワード"
-              fullWidth
-              InputLabelProps={{ className: "font-mono" }}
-            />
+          <Grid container className="mt-10">
+            <Grid item className="w-96">
+              <FormPasswordTextField
+                name="password"
+                label="パスワード"
+                helperText={signUpForm.formState.errors.password?.message}
+                InputLabelProps={{ className: "font-mono" }}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container className="mt-10">
-          <Grid item className="w-96">
-            <TextField
-              name="passwordConfirmation"
-              label="パスワード確認"
-              fullWidth
-              InputLabelProps={{ className: "font-mono" }}
-            />
+          <Grid container className="mt-10">
+            <Grid item className="w-96">
+              <FormPasswordTextField
+                name="passwordConfirmation"
+                label="パスワード確認"
+                helperText={signUpForm.formState.errors.passwordConfirmation?.message}
+                InputLabelProps={{ className: "font-mono" }}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container className="justify-center mt-10">
-          <Grid item>
-            <Button
-              className="text-gray-500 border-gray-500 font-mono"
-              variant="text"
-              sx={{
-                height: "60px",
-                width: "130px",
-              }}
-            >
-              登録
-            </Button>
+          <Grid container className="justify-center mt-10">
+            <Grid item>
+              <Button
+                className="text-gray-500 border-gray-500 font-mono"
+                variant="text"
+                onClick={signUpForm.handleSubmit(handleSignUpOnClickTest)}
+                sx={{
+                  height: "60px",
+                  width: "130px",
+                }}
+              >
+                登録
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </FormProvider>
     </>
   );
 };
